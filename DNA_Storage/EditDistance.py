bce05561b4b50af75d552c7a97ad20d915b661eb
ylu1997 <45112420+ylu1997@users.noreply.github.com>
2023-11-05 13:30:16 +0800

    Update and rename EditDistance to EditDistance.py

---
+++ DNA_Storage/EditDistance.py
@@ -0,0 +1,4 @@
+#This file is about editing distance calculation and their device function over GPU
+#    Include: Insertion and deletion number 
+#             Levenshtein distance
+#             Refined levenshtein distance
